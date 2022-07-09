d5d89b0c0539128aeb17de38bff1f2986543cdb1
daniel-fudge <daniel.fudge1977@gmail.com>
2022-07-09 19:49:41 -0400

    added amplify folder

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsamplifyreactdemo662a8d78": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -21,5 +21,4 @@ function App({ signOut }) {
     </View>
   );
 }
-
 export default withAuthenticator(App);
